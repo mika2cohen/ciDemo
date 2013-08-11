311ed97268f6ca0cd45fc7b38f803310d35dddd8
mika2cohen <mika2cohen@gmail.com>
2013-08-11 19:18:45 +0300

    Tuto Basic Calculator http://www.youtube.com/watch?v=UY8tCAdgcBE

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -49,7 +49,13 @@ class Site extends CI_Controller {
 
     public function home(){
         $data['title'] = "Welcome!";
-       $this->load->view("view_home", $data);
+        $data['var1'] = 1;
+        $data['var2'] = 2;
+
+        $this->load->model("math");
+        $data['addTotal'] = $this->math->add($data["var1"],$data["var2"]);
+        $data['subTotal'] = $this->math->sub($data["var1"],$data["var2"]);
+        $this->load->view("view_home", $data);
     }
 
     public function addStuff(){
--- application/views/view_home.php
+++ application/views/view_home.php
@@ -68,6 +68,10 @@
 
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>
+    <h2>Add</h2>
+    <p><?php echo $var1. " + ". $var2. " = " . $addTotal; ?></p>
+    <h2>Sub</h2>
+    <p><?php echo $var1. " - ". $var2. " = " . $subTotal; ?></p>
 
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
